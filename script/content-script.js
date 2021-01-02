5617e184a9c0cc1d967acad7b96baf0e1a4f776a
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-02 20:13:56 +0530

    Logging open tabs to console

--- script/content-script.js
+++ script/content-script.js
@@ -2,7 +2,7 @@ document.getElementById("urls-group-title").addEventListener("keypress",
     function(e){
         if(e.key === "Enter"){
             // broadcasts a message to any listener that we have instantiated in our chrome extension
-            chrome.runtime.sendMessage({addUrls: true})
+            chrome.runtime.sendMessage({title: e.target.value})
         }
     }
 );
--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
 chrome.runtime.onMessage.addListener(
     function(request){
-        if(request.addUrl){
+        if(request.title){
+            chrome.windows.getCurrent({populate:true}, (window)=>{
+                // takes all the tabs on window and filtering all the general chrome tabs
+                
+                // The indexOf() method returns the position of the first occurrence of a specified value in a string.
+                // This method returns -1 if the value to search for never occurs.
+                // Note: The indexOf() method is case sensitive.
+                const urls = window.tabs.filter( t=> t.url.indexOf("chrome://") !=0);
+                console.log(urls);
+            });
             // Store New URL's
         }
     }
