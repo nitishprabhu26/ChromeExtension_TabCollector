33271f2d7fbfa63fe96f023dada3fa02b803e606
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-02 23:57:33 +0530

    Displaying URL's in the popup HTML file

--- script/content-script.js
+++ script/content-script.js
@@ -2,7 +2,29 @@ document.getElementById("urls-group-title").addEventListener("keypress",
     function(e){
         if(e.key === "Enter"){
             // broadcasts a message to any listener that we have instantiated in our chrome extension
-            chrome.runtime.sendMessage({title: e.target.value})
+            chrome.runtime.sendMessage({title: e.target.value});
         }
     }
 );
+
+chrome.storage.sync.get(["urlLists"], (obj)=>{
+    console.log(obj);
+    if(obj.urlLists.length){
+        const htmlLists = obj.urlLists.map((list) => {
+            const container = document.createElement("div");
+            container.setAttribute("class", "title");
+            container.innerHTML=list.title;
+
+            list.urls.forEach((element) => {
+                const div = document.createElement("div");
+                div.innerHTML = element.url;
+                container.appendChild(div);
+            });
+            return container;
+        });
+
+        htmlLists.forEach((list)=>{
+            document.getElementById("lists-container").appendChild(list);
+        });
+    }
+});
